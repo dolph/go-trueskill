97652ae14c636fc5d2262dc0ff529760fe1bd00b
Dolph Mathews <dolph.mathews@gmail.com>
2024-08-17 08:02:32 -0500

    simplify boolean return

--- mathextra/float.go
+++ mathextra/float.go
@@ -4,8 +4,5 @@ import "math"
 
 // Float64AlmostEq returns true if the difference between two floats is less than or equal to epsilon (ε).
 func Float64AlmostEq(a, b, ε float64) bool {
-	if math.Abs(a-b) <= ε {
-		return true
-	}
-	return false
+	return math.Abs(a-b) <= ε
 }
